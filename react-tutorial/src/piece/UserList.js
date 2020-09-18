faed52689474f94a027fc57c4f8df0b4e7fe0e05
HongNyungKi <hnk2017@naver.com>
2020-09-18 21:56:31 +0900

    review

--- react-tutorial/src/piece/UserList.js
+++ react-tutorial/src/piece/UserList.js
@@ -1,20 +1,38 @@
 import React from 'react';
 
+const users = [
+    {
+        id:1,
+        username:'홍녕기',
+        email:'hnk2017@naver.com'
+    },{
+        id:2,
+        username:'양현준',
+        email:'akakak231@google.com'
+    },{
+        id:3,
+        username:'김철환',
+        email:'kkcch9@naver.com'
+    }
+]
+
 function User({person}){
-    return (
-        <div>
-            {person.username} : {person.email}
-            <button>삭제</button>
-        </div>
+    return(
+    <div>
+        {person.username} : {person.email}
+    </div>
     )
 }
 
-function UserList({userInfo}){
-    return (
+function UserList(){
+    
+    return(
         <div>
-            {userInfo.map(
-                (user)=>(<User person={user} key={user.id}/>)
-            )}
+            {
+                users.map(
+                    (user)=>(<User person={user} key={user.id}/>)
+                )
+            }
         </div>
     )
 }
